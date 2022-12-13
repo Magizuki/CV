7e81c614ad9686f1e7a61e7d592fce17911bba5d
christoper.irvine <christoper.irvine@imfi.com>
2022-12-13 09:37:25 +0700

    add resource and new resume page

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,38 +6,39 @@ export default function Introduction() {
         <React.Fragment>
             <Navigation width={window.innerWidth} />
             <div className="w-full">
-            <span className="text-xl font-bold lg:text-3xl">About Me</span>
-            <br />
-            <br />
-            <hr className="w-20 lg:w-40 h-1 font-bold bg-blue-600 border-none rounded-md" />
-            <br />
-            <span>
-              Hello, my name is Christopher Irvine Sendjaya with Irvine as my nickname. 
-              I am fresh graduate from Binus University in Computer Science major with 3.63 GPA. 
-              I am from duren sawit, east jakarta. 
-              Currently i work in PT. Indomobil Finance Indonesia as IT Supporting Business Application Staff. 
-              For more detail about me, can be seen in my cv.
-            </span>
-            <br />
-            <br />
-            <span className="text-xl font-bold lg:text-3xl">What I'm Doing</span>
-            <br />
-            <br />
-            <hr className="w-20 lg:w-40 h-1 font-bold bg-blue-600 border-none rounded-md" />
-            <br />
-            <div className="lg:flex-row block">
-              <div className="lg:flex block gap-6 w-full">
-                  <div className="flex items-center rounded-lg lg:w-1/2 w-full h-1/5 gap-3 p-5 bg-white dark:bg-[#302C40] lg:bg-[#F5F6F7] text-[#44566C] lg:dark:bg-[#272333] dark:text-[#DEDDE0]">
-                    <img src="assets/icons/website/website.png" className="hover:cursor-pointer w-12 lg:w-1/3 lg:h-24 h-16" alt="Web" />
-                    <span className="text-2xl ml-10 mt-5">Web Development</span>
-                  </div>
-                  <div className="flex items-center rounded-lg lg:w-1/2 w-full h-1/5 gap-3 p-5 bg-white dark:bg-[#302C40] lg:bg-[#F5F6F7] text-[#44566C] lg:dark:bg-[#272333] dark:text-[#DEDDE0]">
-                    <img src="assets/icons/database/database.png" className="hover:cursor-pointer w-12 lg:w-1/3 lg:h-24 h-16" alt="Web" />
-                    <span className="text-2xl ml-10 mt-5">Database Management</span>
-                  </div>
+              {window.innerWidth < 800 ? (<React.Fragment><br /><br /></React.Fragment>) : ""}
+              <span className="text-xl font-bold lg:text-3xl">About Me</span>
+              <br />
+              <br />
+              <hr className="w-20 lg:w-40 h-1 font-bold bg-blue-600 border-none rounded-md" />
+              <br />
+              <span>
+                Hello, my name is Christopher Irvine Sendjaya with Irvine as my nickname. 
+                I am fresh graduate from Binus University in Computer Science major with 3.63 GPA. 
+                I am from duren sawit, east jakarta. 
+                Currently i work in PT. Indomobil Finance Indonesia as IT Supporting Business Application Staff. 
+                For more detail about me, can be seen in my cv.
+              </span>
+              <br />
+              <br />
+              <span className="text-xl font-bold lg:text-3xl">What I'm Doing</span>
+              <br />
+              <br />
+              <hr className="w-20 lg:w-40 h-1 font-bold bg-blue-600 border-none rounded-md" />
+              <br />
+              <div className="lg:flex-row block">
+                <div className="lg:flex block gap-6 w-full">
+                    <div className="flex items-center rounded-lg lg:w-1/2 w-full h-1/5 gap-3 p-5 bg-white dark:bg-[#302C40] lg:bg-[#F5F6F7] text-[#44566C] lg:dark:bg-[#272333] dark:text-[#DEDDE0]">
+                      <img src="assets/icons/website/website.png" className="hover:cursor-pointer w-12 lg:w-1/3 lg:h-24 h-16" alt="Web" />
+                      <span className="text-2xl ml-10 mt-5">Web Development</span>
+                    </div>
+                    <div className="flex items-center rounded-lg lg:w-1/2 w-full h-1/5 gap-3 p-5 bg-white dark:bg-[#302C40] lg:bg-[#F5F6F7] text-[#44566C] lg:dark:bg-[#272333] dark:text-[#DEDDE0]">
+                      <img src="assets/icons/database/database.png" className="hover:cursor-pointer w-12 lg:w-1/3 lg:h-24 h-16" alt="Web" />
+                      <span className="text-2xl ml-10 mt-5">Database Management</span>
+                    </div>
+                </div>
               </div>
             </div>
-          </div>
         </React.Fragment>
     );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,6 +5,23 @@ export default function Resume(){
     return (
         <React.Fragment>
             <Navigation width={window.innerWidth} />
+            {window.innerWidth < 800 ? (<React.Fragment><br /><br /></React.Fragment>) : ""}
+            <div className="w-full flex">
+                <div className="flex w-1/2">
+                    <img src="assets/icons/education/education.png" />
+                    <span className="text-xl font-bold lg:text-3xl">Education</span>
+                    <br />
+                    <br />
+                    
+                </div>
+                <div className="flex w-1/2">
+                    <img src="assets/icons/experience/experience.png" />
+                    <span className="text-xl font-bold lg:text-3xl">Experience</span>
+                    <br />
+                    <br />
+                    
+                </div>           
+            </div>
         </React.Fragment>
     )
 }
